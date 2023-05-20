e7c726cb4a0e37646a707b22c1d59fb58de3880e
Ben Corner <ben.corner88@gmail.com>
2023-05-20 16:54:32 -0600

    addition of python package

---
+++ examples/driver.py
@@ -0,0 +1,13 @@
+
+import numpy as np
+
+from my_python_package.operators import (
+	add,
+)
+
+def main():
+
+	print(f'add(3,5): {add(3,5)}')
+
+if __name__ == '__main__':
+	main()
---
+++ src/my_python_package/operators.py
@@ -0,0 +1,23 @@
+
+
+import numpy as np
+
+def add(x,y):
+	"""Add 2 numbers or arrays.
+
+	Parameters
+	_________
+	x : int or float or array_like
+	    First number to add
+	y : int or float or array_like
+	    Second number to add
+
+	Returns
+	_______
+	int or float or array_like
+	Sum of x and y
+	"""
+
+	return x + y
+
+
